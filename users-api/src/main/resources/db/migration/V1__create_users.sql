a36e39ee920e05f928da469690bef465a88b9b2c
David Hernandez <david.hernandez8015@gmail.com>
2019-11-09 19:36:33 -0500

    corrected yml and applications-dev.properties files

--- users-api/src/main/resources/db/migration/V1__create_users.sql
+++ users-api/src/main/resources/db/migration/V1__create_users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id SERIAL,
+    id SERIAL unique,
     email VARCHAR(100) NOT NULL unique,
     password VARCHAR(50) NOT NULL,
     username VARCHAR(50) unique
--- users-api/src/main/resources/db/migration/V2__create_posts.sql
+++ users-api/src/main/resources/db/migration/V2__create_posts.sql
@@ -1,7 +1,7 @@
 CREATE TABLE posts (
-    id SERIAL,
+    id SERIAL unique,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(50) NOT NULL,
-    user_id VARCHAR(100),
+    user_id int,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
