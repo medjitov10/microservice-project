b84fec10a3a178e2955b481db3d92deac79ec826
medjitov10 <medjitov10@gmail.com>
2019-11-10 19:17:56 -0500

    fixed user db

--- users-api/src/main/resources/db/migration/V1__create_users.sql
+++ users-api/src/main/resources/db/migration/V1__create_users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
-    id SERIAL unique,
-    email VARCHAR(100) NOT NULL unique,
-    password VARCHAR(50) NOT NULL,
-    username VARCHAR(50) unique
+    id SERIAL,
+    email VARCHAR(100) NOT NULL,
+    username VARCHAR(50),
+    password VARCHAR(50)
 );
--- users-api/src/main/resources/db/migration/V2__create_posts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE posts (
-    id SERIAL unique,
-    title VARCHAR(100) NOT NULL,
-    description VARCHAR(50) NOT NULL,
-    user_id int,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
