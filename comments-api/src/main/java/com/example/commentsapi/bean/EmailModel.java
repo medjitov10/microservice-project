36968bcb9456e89b4c7f7c139dacb1066140897b
David Hernandez <david.hernandez8015@gmail.com>
2019-12-04 11:41:01 -0500

    edited comments api

--- comments-api/src/main/java/com/example/commentsapi/bean/EmailModel.java
+++ comments-api/src/main/java/com/example/commentsapi/bean/EmailModel.java
@@ -1,12 +1,54 @@
 package com.example.commentsapi.bean;
 
 
-public class EmailModel {
+import java.io.Serializable;
+
+public class EmailModel implements Serializable {
     private String authorEmail;
     private String postTitle;
     private String commentText;
     private String authorUsername;
     private String CommentUsername;
+
+    public String getAuthorEmail() {
+        return authorEmail;
+    }
+
+    public void setAuthorEmail(String authorEmail) {
+        this.authorEmail = authorEmail;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public String getAuthorUsername() {
+        return authorUsername;
+    }
+
+    public void setAuthorUsername(String authorUsername) {
+        this.authorUsername = authorUsername;
+    }
+
+    public String getCommentUsername() {
+        return CommentUsername;
+    }
+
+    public void setCommentUsername(String commentUsername) {
+        CommentUsername = commentUsername;
+    }
 
     public EmailModel(String authorEmail, String postTitle, String commentText, String authorUsername, String commentUsername) {
         this.authorEmail = authorEmail;
